02c5828d1008bf1a8ef5e2777ff3438ea8ee36ed
goshva <sha.egor@gmail.com>
2022-11-08 13:48:49 +0300

    add simple modal

--- src/examples/cards/blogCards/CenteredBlogCard.vue
+++ src/examples/cards/blogCards/CenteredBlogCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import MaterialButton from "@/components/MaterialButton.vue";
+
 defineProps({
   image: {
     type: String,
@@ -38,9 +40,63 @@ defineProps({
       <p class="mb-0">
         {{ description }}
       </p>
-      <button type="button" class="btn btn-sm mb-0 mt-3" :class="action.color">
-        {{ action.label }}
-      </button>
+
+      <MaterialButton
+        variant="gradient"
+        color="success"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        ПЕРЕЙТИ
+      </MaterialButton>
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Your modal title
+              </h5>
+              <MaterialButton
+                color="none"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+              </MaterialButton>
+            </div>
+            <div class="modal-body">
+              Society has put up so many boundaries, so many limitations on
+              what’s right and wrong that it’s almost impossible to get a pure
+              thought out.
+              <br /><br />
+              It’s like a little kid, a little boy, looking at colors, and no
+              one told him what colors are good, before somebody tells you you
+              shouldn’t like pink because that’s for girls, or you’d instantly
+              become a gay two-year-old.
+            </div>
+            <div class="modal-footer justify-content-between">
+              <MaterialButton
+                variant="gradient"
+                color="dark"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </MaterialButton>
+              <MaterialButton variant="gradient" color="success" class="mb-0">
+                Save changes
+              </MaterialButton>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Modal -->
     </div>
   </div>
 </template>
