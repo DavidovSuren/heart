74225d9318fcb47e99cf16b0fd67450996682c08
lowa <lowa@lowa>
2022-11-11 12:38:52 +0000

    del

--- src/views/Presentation/Components/BuiltByDevelopers.vue
+++ src/views/Presentation/Components/BuiltByDevelopers.vue
@@ -29,33 +29,4 @@
       </div>
     </div>
   </div>
-  <div class="container mt-sm-5">
-    <div
-      class="page-header py-6 py-md-5 my-sm-3 mb-3 border-radius-xl"
-      :style="{
-        backgroundImage:
-          'url(src/assets/img/mass.jpg)'
-      }"
-      loading="lazy"
-    >
-      <span class="mask bg-gradient-green"></span>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 ms-lg-5">
-            <h4 class="text-white">Все виды массажа</h4>
-            <h1 class="text-white">В КУРОРТНОМ ГОРОДЕ</h1>
-            <p class="lead text-white opacity-8">
-             На дому , выезд..
-            </p>
-            <a
-              class="text-white icon-move-right"
-            >
-              ПЕРЕЙТИ
-              <i class="fas fa-arrow-right text-sm ms-1"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
