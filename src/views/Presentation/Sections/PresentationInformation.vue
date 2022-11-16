86085ebf4581b140a6b1a5e828df8065b6278a59
lowa <lowa@lowa>
2022-11-16 16:36:07 +0000

    11

--- src/views/Presentation/Sections/PresentationInformation.vue
+++ src/views/Presentation/Sections/PresentationInformation.vue
@@ -16,7 +16,6 @@ import imageSrc from "@/assets/img/Gorod.jpeg"
               image="https://images.unsplash.com/photo-1569683795645-b62e50fbf103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
               icon="touch_app"
               title="Афиша <br />"
-              description=""
             />
 
             <RotatingCardBack
