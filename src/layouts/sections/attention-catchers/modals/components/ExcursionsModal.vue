514d115c9f22aa2908e0fdd469ac494cfe333e83
lowa <lowa@lowa>
2022-11-16 16:07:15 +0000

    card экскурси беспла

--- src/layouts/sections/attention-catchers/modals/components/ExcursionsModal.vue
+++ src/layouts/sections/attention-catchers/modals/components/ExcursionsModal.vue
@@ -18,11 +18,8 @@
         >
           <div class="modal-dialog">
             <div class="modal-content">
-              <div class="modal-header">
-                <img src="@/assets/img/adm.jpeg" alt="" width="100" height="100">
-                <h5 class="modal-title" id="exampleModalLabel">
-                  Администрация города-курорта Кисловодск 
-                </h5>
+              <div>
+            
                 <MaterialButton
                   color="none"
                   class="btn-close"
@@ -30,7 +27,11 @@
                   aria-label="Close">
                 </MaterialButton>
               </div>
-              <div class="modal-body" color="green">
+              <div class="modal-body" color="green"
+              ><img src="@/assets/img/adm.jpeg" >
+              <h5 class="modal-title" id="exampleModalLabel">
+                  Администрация города-курорта Кисловодск 
+                </h5>
                 Предоставляет 30 бесплатных билетов, на пешеходную экскурсию по исторической части города курорта Кисловодска еженедельно, успей забрать.<br><br>
                Свяжись в 
       <a href="https://wa.me/89187835805>"
